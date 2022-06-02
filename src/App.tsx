f54a01532896c761e49b1d4e4b0b57ae3da414e6
oriverk <oriveryk@gmail.com>
2022-06-02 17:40:57 +0900

    modify app.tsx and main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { Layout } from "./components/Layout";
-import { Home } from "./pages/Home";
-import { VectorLayers } from "./pages/VectorLayers";
-import { Events } from "./pages/Events";
-import { PolylineMarker } from "./pages/PolylineMarker";
-import { TaiwanPaths } from "./pages/taiwan";
+import React from "react";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { Layout } from "./components/Layouts"
+import Home from "./pages/Home";
+import CustomMarker from "./pages/Marker"
+import VectorLayers from "./pages/VectorLayers"
+import Events from "./pages/Events"
+import PolylineMarker from "./pages/PolylineMarker"
+import GeoJsonComponent from "./pages/Geojson"
+import Taiwan from "./pages/Taiwan"
+
 
 import "leaflet/dist/leaflet.css";
-import "./App.css"
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="marker" element={<CustomMarker />} />
         <Route path="vector-layers" element={<VectorLayers />} />
         <Route path="events" element={<Events />} />
         <Route path="polyline-marker" element={<PolylineMarker />} />
-        <Route path="taiwan" element={<TaiwanPaths />} />
+        <Route path="geo" element={<GeoJsonComponent />} />
+        <Route path="taiwan" element={<Taiwan />} />
       </Route>
     </Routes>
   </BrowserRouter>
