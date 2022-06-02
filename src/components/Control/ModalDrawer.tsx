38b937a76870fb2f4b069e08726a25226c85bbc6
oriverk <oriveryk@gmail.com>
2022-06-02 17:38:40 +0900

    create Control dir for map control modal component

---
+++ src/components/Control/ModalDrawer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+type Props = {
+  className?: string;
+  children: React.ReactNode;
+  display: boolean;
+}
+
+export const ModalDrawer: React.FC<Props> = (props) => {
+  const { className, children, display } = props;
+
+  return (
+    <Box className={className}
+      zIndex={1000} pos="absolute" top={16} right={0}
+      p={4} h="calc(100% - 64px)"
+      display={display ? "block" : "none"}
+    >
+      <Box w="300px" h="100%"
+        bg="white" color="gray.800" borderRadius={16}
+        overflowY="hidden"
+      >
+        <Box p={4} w="100%" h="100%" overflowY="auto">
+          {children}
+        </Box>
+      </Box>
+    </Box>
+  )
+}
---
+++ src/components/Control/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Box, Text, Checkbox, useBreakpointValue } from '@chakra-ui/react'
+import GpxParser from 'gpxparser'
+
+import { useModal } from '../../components/Layouts';
+import { ModalDrawer } from "./ModalDrawer"
+import { Overlay } from './Overlay';
+
+type Props = {
+  parsedGpxData: GpxParser[]
+}
+
+export const Control: React.FC<Props> = (props) => {
+  const { parsedGpxData } = props;
+
+  const { isModalOpen, setModalOpen } = useModal()
+  const isXlPc = useBreakpointValue({ base: false, xl: true })
+  const isOverlay = !isXlPc && isModalOpen;
+
+  return (
+    <>
+      {isOverlay && (
+        <Overlay onClick={() => setModalOpen(false)} />
+      )}
+      <ModalDrawer display={isModalOpen}>
+        {parsedGpxData.map(datum => {
+          const { tracks } = datum;
+          return tracks.map(track => {
+            const { name } = track;
+            return (
+              <>
+                <Box mx="auto" px={2} w="95%" h={12}
+                  borderRadius={16}
+                  _hover={{ backgroundColor: "blackAlpha.200" }}
+                  key={name}
+                >
+                  <Text as="h3">
+                    <Checkbox defaultChecked fontSize={3}>{name}</Checkbox>
+                  </Text>
+                </Box>
+              </>
+            )
+          })
+        })}
+      </ModalDrawer>
+    </>
+  )
+}
